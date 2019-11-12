4031d65839f5146e50665e28478c6fd192d45661
Darshan Chheda <ddc@somaiya.edu>
2019-11-12 15:03:47 +0530

    New Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WindowComponent } from './window/window.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WindowComponent
   ],
   imports: [
     BrowserModule,
